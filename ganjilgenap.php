9696e3cc7205e7540cc98796c8f90c97d0f40068
FeriansyahRifai <55324660+FeriansyahRifai@users.noreply.github.com>
2019-09-29 16:32:41 +0700

    Add files via upload

---
+++ ganjilgenap.php
@@ -0,0 +1,9 @@
+<?php 
+for ($no=1; $no<=25; $no++){
+    if ($no % 2 == 0)
+        echo "$no = <span style='color:blue'>Bilangan Genap</span><br>";
+    else
+        echo "$no = Bilangan Ganjil<br>";
+}
+
+?>
